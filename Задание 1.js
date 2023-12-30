c027a87427b42c7adf45badc4af2ba7db353291e
vodogrei13 <vodogrei15@mail.ru>
2023-12-30 16:57:42 +0300

    second commit

--- Задание 1.js
+++ Задание 1.js
@@ -1,8 +1,8 @@
 let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-let even = 0;
-let odd = 0;
-let zeros = 0;
 function getSumEvenOddNumbers() {
+  let even = 0;
+  let odd = 0;
+  let zeros = 0;
   for (let i = 0; i < arr.length; i++) {
   if (arr[i] === 0) {
     zeros++
